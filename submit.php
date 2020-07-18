a0cf74cbe5b354668dc3095df4bec5df96629dc6
No need to know my name <68473565+EmiX1996@users.noreply.github.com>
2020-07-18 19:28:44 +0200

    Quick load
    Quick load

---
+++ submit.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__.'/maincore.php';
+if (!iMEMBER) {
+    redirect(BASEDIR.'index.php');
+}
+
+require_once THEMES.'templates/header.php';
+$modules = \PHPFusion\Admins::getInstance()->getSubmitData();
+
+$_GET['stype'] = !empty($_GET['stype']) && isset($modules[$_GET['stype']]) ? $_GET['stype'] : "";
+
+if (!empty($modules) && $_GET['stype']) {
+    require_once $modules[$_GET['stype']]['link'];
+} else {
+    redirect(BASEDIR.'index.php');
+}
+
+require_once THEMES.'templates/footer.php';
